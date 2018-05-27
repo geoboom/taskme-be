3c87075d51a483c60e1162b039639c4cb2a11269
geoboom <leegeorgie52@gmail.com>
2018-05-28 03:42:30 +0800

    asdsa

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 
 app.use('/api', router);
-app.get('/', (req, res, next) => res.send('taskme api'));
+app.get('/', (req, res, next) => res.send('taskme api\ndl: https://drive.google.com/open?id=1ejCwlSATsQsGoDr6ruxn9eqTpSICYIzv'));
 
 errorHandler(app);
 
