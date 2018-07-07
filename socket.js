f6551d129b696a21992d2abe9293102aa886574a
geoboom <leegeorgie52@gmail.com>
2018-07-07 21:09:17 +0800

    21321

--- socket.js
+++ socket.js
@@ -22,6 +22,7 @@ const init = (server) => {
       console.log('disconnected');
       try {
         await unregisterPresence(socket);
+        console.log('unregistered');
         socket.emit('unregistered');
       } catch (e) {
         console.log(e);
