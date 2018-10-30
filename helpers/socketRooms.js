a2c4e97675e61b98a79a3811b9dae9f460ac361d
geoboom <leegeorgie52@gmail.com>
2018-10-30 10:26:49 +0800

    length check before iteration

--- helpers/socketRooms.js
+++ helpers/socketRooms.js
@@ -1,6 +1,6 @@
 exports.deleteRoom = (io, roomName) => {
   const room = io.sockets.adapter.rooms[roomName];
-  if (room && room.sockets) { // room.sockets.forEach is not a function
+  if (room && room.sockets && room.sockets.length > 0) { // room.sockets.forEach is not a function
     room.sockets.forEach((s) => {
       s.leave(room);
     });
